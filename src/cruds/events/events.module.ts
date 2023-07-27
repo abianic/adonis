78a2f6ef9cecb184f71d95db262dbc1e1d95515b
Mauricio Loayza <mauricioloayzas@mauloasan.com>
2023-07-27 16:43:32 -0500

    Resolving conflicts

--- src/cruds/events/events.module.ts
+++ src/cruds/events/events.module.ts
@@ -5,6 +5,7 @@ import { EventTypesController } from './event-types.controller';
 import { EventTypesService } from './event-types.service';
 import { Profile } from '../profiles/profile.entity';
 
+
 @Module({
   imports: [TypeOrmModule.forFeature([EventType, Profile])],
   controllers: [EventTypesController],
